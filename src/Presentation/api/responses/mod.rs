682135e3a91495eac239d26976a34facbba126ef
Wilfer <dwilfer@gmail.com>
2025-04-01 16:50:52 -0500

    Aja

---
+++ src/Presentation/api/responses/mod.rs
@@ -0,0 +1,6 @@
+// Este módulo contiene las estructuras y funciones para las respuestas de la API.
+pub mod api_response;
+pub mod api_error;
+
+pub use api_response::ApiResponse;
+pub use api_error::ApiError;
